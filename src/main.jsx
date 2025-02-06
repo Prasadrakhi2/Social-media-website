919f4a6fb1e5c6cab6e492d216fd7bea0c7fdcf3
rakhi <prasadrakhi20004@gmail.com>
2025-02-06 09:58:16 +0530

    routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
+import CreatePost from './Components/CreatePost.jsx'
+
+const router = createBrowserRouter([
+  {path : "/", element : <App/>},
+  {path : "/create-post" , element : <CreatePost/>}
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router}/>
   </StrictMode>,
 )
